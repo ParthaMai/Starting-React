4283fd3ab281d4a0ac7cd01272b1dd55b99099b4
Partha Maity <parthamaity2004@gmail.com>
2026-01-15 00:51:05 +0530

    Now implement swap currency

--- Project/06CurrencyConverter/src/App.jsx
+++ Project/06CurrencyConverter/src/App.jsx
@@ -1,8 +1,26 @@
 import { useState } from 'react'
-
+import { InputBox } from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const[from,setFrom] = useState("usd")
+  const[to, setTo] = useState("inr")
+  const[convertedAmount, setConverterdAmount] = useState(0)
+
+
+  const currencyInfo = useCurrencyInfo(from) // means in useCurrencyinfo hook pass from means usd to get all usd convertion value 
+
+  const options = object.keys(currencyInfo) // to get only the keys like inr, 
+
+  // For swaping the convertion
+  const swap = () =>{
+    //const temp = from ; // don’t need to this line //•	React groups multiple state updates together and applies them in a single re-render
+    setFrom(to);
+    setTo(from);
+    setConverterdAmount(amount); // for this value will be swap, so this is optional
+    setAmount(convertedAmount)
+  }
 
   return (
     <>
